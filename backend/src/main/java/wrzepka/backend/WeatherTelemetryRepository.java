9a3fbaad83eb36bb814f1afc4c53902c6a82f0de
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-07-09 00:09:56 +0200

    feat: create WeatherTelemetry repository

---
+++ backend/src/main/java/wrzepka/backend/WeatherTelemetryRepository.java
@@ -0,0 +1,8 @@
+package wrzepka.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherTelemetryRepository extends JpaRepository<WeatherTelemetry, WeatherTelemetryId> {
+}
