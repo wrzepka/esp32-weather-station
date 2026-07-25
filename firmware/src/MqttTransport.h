b1c10c78f098a0d44b2dffbf887e0ea420528d72
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-07-25 23:19:14 +0200

    docs(mqtt): document class fields

--- firmware/src/MqttTransport.h
+++ firmware/src/MqttTransport.h
@@ -8,23 +8,44 @@
 
 #include "ITelemetryTransport.h"
 
-
+/**
+ * TODO
+ */
 class MqttTransport : public ITelemetryTransport {
 private:
-    const char *m_brokerIp;
-    uint16_t m_brokerPort;
-    esp_mqtt_client_handle_t m_client;
+    const char *m_brokerIp;/**<Mqtt broker IP address*/
+    uint16_t m_brokerPort;/**< Mqtt broker port (default: 1833)*/
+    esp_mqtt_client_handle_t m_client;/**< Mqtt client handle*/
 
+    /**
+     * @brief TODO
+     * @param handler_args
+     * @param base
+     * @param event_id
+     * @param event_data
+     */
     static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 public:
     MqttTransport(const char *brokerIp, uint16_t brokerPort) : m_brokerIp(brokerIp), m_brokerPort(brokerPort), m_client(nullptr) {
     };
 
+    /**
+     * TODO
+     * @return
+     */
     esp_err_t connect() override;
 
+    /**
+     * TODO
+     * @return
+     */
     esp_err_t disconnect() override;
 
+    /**
+     * TODO
+     * @return
+     */
     esp_err_t publish() override;
 };
 
