3ff9958c6a8aec765cf0050c83ddc9489fec6c7e
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-09 20:05:16 +0200

    docs(SensorManager): document methods using doxygen format

--- firmware/src/SensorManager.h
+++ firmware/src/SensorManager.h
@@ -9,17 +9,44 @@
 
 class SensorManager {
 private:
-    BME280 bme280;
-    BH1750 bh1750;
-    public:
-        struct Payload {
-            uint32_t light_intensity;
-            BME280::Data bme_data;
-        };
+    BME280 bme280; /**<Sensor measuring temperature, pressure and humidity.*/
+    BH1750 bh1750; /**<Sensor measuring light intensity.*/
+public:
+    /**
+     * @brief structure used for transferring sensor data via transport protocol.
+     */
+    struct Payload {
+        uint32_t light_intensity;
+        BME280::Data bme_data;
+    };
 
-        SensorManager(i2c_master_bus_handle_t i2c_master_bus_handle);
-        esp_err_t getPayload(Payload& payload);
-        static constexpr uint32_t get_measurement_delay_in_ms();
+    /**
+     * @brief Initializes sensors.
+     *
+     * Initializes all sensors (BH1750, BME280) by using begin() methods.
+     *
+     * @param i2c_master_bus_handle
+     */
+    SensorManager(i2c_master_bus_handle_t i2c_master_bus_handle);
+
+    /**
+     * @brief triggers measurements and then fetches data.
+     *
+     * Triggers sensors measurement. If at least one measurement succeed, fetches data from that sensor and saves it to
+     * Payload structure. Otherwise, fill Payload fields with value sentinels (ex. INT_32_MAX).
+     *
+     * @param[out] payload reference to Payload structure. It contains measurement data.
+     * @return ESP_OK if fetches data from at least one sensor.
+     * @return ESP_FAIL if all sensors are down.
+     */
+    esp_err_t getPayload(Payload &payload);
+
+    /**
+     * @brief Compares and returns the highest measurement delay.
+     *
+     * @return the highest measurement delay.
+     */
+    static constexpr uint32_t get_measurement_delay_in_ms();
 };
 
 #endif //FIRMWARE_SENSORMANAGER_H
