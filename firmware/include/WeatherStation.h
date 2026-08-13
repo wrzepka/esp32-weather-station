f1f0ae0041793163817cb7c87b22284e772bec2b
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-13 20:41:43 +0200

    feat: create boilerplate for WeatherStation class

---
+++ firmware/include/WeatherStation.h
@@ -0,0 +1,31 @@
+//
+// Created by Wiktor on 13.08.2026.
+//
+
+#ifndef FIRMWARE_WEATHERSTATION_H
+#define FIRMWARE_WEATHERSTATION_H
+#include <esp_err.h>
+#include <driver/i2c_types.h>
+
+#include "MqttTransport.h"
+#include "SensorManager.h"
+#include "WiFiManager.h"
+
+//TODO: docs
+class WeatherStation {
+    SensorManager::Payload current_payload{};
+    SensorManager sensor_manager;
+    WiFiManager wifi_manager;
+    MqttTransport mqtt_transport;
+
+public:
+    WeatherStation(i2c_master_bus_handle_t i2c_bus_handle, const char* brokerIp, uint16_t brokerPort): sensor_manager(i2c_bus_handle), mqtt_transport(brokerIp, brokerPort){};
+
+    esp_err_t init();
+    esp_err_t measure();
+    esp_err_t send();
+    esp_err_t sleep();
+
+};
+
+#endif //FIRMWARE_WEATHERSTATION_H
---
+++ firmware/src/WeatherStation.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Wiktor on 13.08.2026.
+//
+
+#include "WeatherStation.h"
+
+esp_err_t WeatherStation::init() {
+    //TODO: method
+    return ESP_OK;
+};
+
+esp_err_t WeatherStation::measure() {
+    //TODO: method
+    return ESP_OK;
+}
+
+esp_err_t WeatherStation::send() {
+    //TODO: method
+    return ESP_OK;
+}
+
+esp_err_t WeatherStation::sleep() {
+    //TODO: method
+    return ESP_OK;
+}
+
