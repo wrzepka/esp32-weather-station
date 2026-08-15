7bd43149dbaf6b6eeeced718c4bb681d5c266cf4
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-15 16:54:33 +0200

    docs(IPayloadSerializer): create documentation

--- firmware/include/IPayloadSerializer.h
+++ firmware/include/IPayloadSerializer.h
@@ -1,7 +1,9 @@
-//
-// Created by Wiktor on 11.08.2026.
-//
-
+/**
+ * @file IPayloadSerializer.h
+ * @author Wiktor
+ * @brief Interface for declaring methods for serializing weather data payload.
+ * @date 11.08.2026
+ **/
 #ifndef FIRMWARE_IPAYLOADSERIALIZER_H
 #define FIRMWARE_IPAYLOADSERIALIZER_H
 #include <esp_err.h>
@@ -9,11 +11,23 @@
 
 #include "SensorManager.h"
 
+/**
+ * @class IPayloadSerializer
+ * @brief Interface for implementing way of serializing weather station telemetry data.
+ *
+ * This interface provides a method that must be implemented for some type of serializer for example in JSON format.
+ */
 class IPayloadSerializer {
 public:
     virtual ~IPayloadSerializer() = default;
 
-    virtual std::string serialize(const SensorManager::Payload&) = 0;
+    /**
+     * @brief Serializes payload into a target representation.
+     *
+     * @param payload reference to Payload structure.
+     * @return Payload serialized to string.
+     */
+    virtual std::string serialize(const SensorManager::Payload& payload) = 0;
 };
 
 #endif //FIRMWARE_IPAYLOADSERIALIZER_H
