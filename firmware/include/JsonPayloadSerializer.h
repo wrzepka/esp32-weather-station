f38d8cab302778b43c18b5191616d59860b6d099
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-16 18:57:54 +0200

    docs(JsonPayloadSerializer): documentation

--- firmware/include/JsonPayloadSerializer.h
+++ firmware/include/JsonPayloadSerializer.h
@@ -1,13 +1,29 @@
-//
-// Created by Wiktor on 11.08.2026.
-//
-
+/**
+ * @file JsonPayloadSerializer.h
+ * @author Wiktor
+ * @brief Class used for serializing payload to JSON format.
+ * @date 11.08.2026
+ **/
 #ifndef FIRMWARE_JSONPAYLOADSERIALIZER_H
 #define FIRMWARE_JSONPAYLOADSERIALIZER_H
 #include "IPayloadSerializer.h"
 
 #endif //FIRMWARE_JSONPAYLOADSERIALIZER_H
 
+/**
+ * @class JsonPayloadSerializer
+ * @brief Class used for serializing weather station payload to JSON format.
+ *
+ * This class provides way o serializing payload to JSON format.
+ */
 class JsonPayloadSerializer: public IPayloadSerializer {
+    /**
+     * @brief Serializes payload to JSON format.
+     *
+     * This method serialize payload data into stringified JSON format using cJSON component.
+     *
+     * @param payload reference to Payload structure.
+     * @return Payload serialized to string in JSON format.
+     */
     std::string serialize(const SensorManager::Payload& payload) override;
 };
