6097c1f5421855bc9bc5831dd2270ee4aae50904
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-16 23:20:37 +0200

    feat(WeatherStation): create init, measure and send logic

--- firmware/src/WeatherStation.cpp
+++ firmware/src/WeatherStation.cpp
@@ -4,24 +4,37 @@
 
 #include "WeatherStation.h"
 
+#include "JsonPayloadSerializer.h"
+// TODO: better error logging? inside of SensorManager?
 esp_err_t WeatherStation::init_sensors() {
-    //TODO: method
-    return ESP_OK;
+    return sensor_manager.init_sensors();
 };
 
 esp_err_t WeatherStation::init_communication() {
-    //TODO: method
-    return ESP_OK;
+    esp_err_t result = wifi_manager.init_wifi_station();
+    if (result != ESP_OK) {
+        return result;
+    }
+
+    return mqtt_transport.connect();
 };
 
 esp_err_t WeatherStation::measure() {
-    //TODO: method
-    return ESP_OK;
+    return sensor_manager.getPayload(current_payload);
 }
 
 esp_err_t WeatherStation::send() {
-    //TODO: method
-    return ESP_OK;
+    JsonPayloadSerializer payload_serializer = JsonPayloadSerializer();
+
+    //Here will be some sort of error checking. If serialization succeed :D
+    std::string serialized_string = payload_serializer.serialize(current_payload);
+
+    esp_err_t init_communication_result = init_communication();
+    if (init_communication_result != ESP_OK) {
+        return init_communication_result;
+    }
+
+    return mqtt_transport.publish(serialized_string);
 }
 
 esp_err_t WeatherStation::sleep() {
