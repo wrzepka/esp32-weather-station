abd28c674d1af5da94d60a912f2014b8634d41a8
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-18 22:47:31 +0200

    docs(WeatherStaion): document part of the class

--- firmware/include/WeatherStation.h
+++ firmware/include/WeatherStation.h
@@ -1,7 +1,9 @@
-//
-// Created by Wiktor on 13.08.2026.
-//
-
+/**
+ * @file WeatherStation.h
+ * @author Wiktor
+ * @brief Class for managing whole app logic such as: sensors, wifi, data transport and deep sleep.
+ * @date 18.08.2026
+ **/
 #ifndef FIRMWARE_WEATHERSTATION_H
 #define FIRMWARE_WEATHERSTATION_H
 #include <esp_err.h>
@@ -11,20 +13,49 @@
 #include "SensorManager.h"
 #include "WiFiManager.h"
 
-//TODO: docs
+/**
+ * @class WeatherStation
+ * @brief Main class for handling whole application logic.
+ *
+ * Handles sensors, Wi-Fi, data transport and deep sleep.
+ */
 class WeatherStation {
-    SensorManager::Payload current_payload{};
-    SensorManager sensor_manager;
-    WiFiManager wifi_manager;
-    MqttTransport mqtt_transport;
+    SensorManager::Payload current_payload{};/**<Current weather data payload.*/
+    SensorManager sensor_manager;/**Instance of sensor manager.*/
+    WiFiManager wifi_manager;/**Instance of Wi-Fi manager.*/
+    MqttTransport mqtt_transport;/**Instance of Wi-Fi manager.*/ //Change to interface ITelemtryTransport?
 
 public:
     WeatherStation(i2c_master_bus_handle_t i2c_bus_handle, const char* brokerIp, uint16_t brokerPort): sensor_manager(i2c_bus_handle), mqtt_transport(brokerIp, brokerPort){};
 
+    /**
+     * TODO: docs after methods polishing.
+     * @return
+     */
     esp_err_t init_sensors();
+
+    /**
+     * TODO: docs after methods polishing.
+     * @return
+     */
     esp_err_t init_communication();
+
+    /**
+     * TODO: docs after methods polishing.
+     * @return
+     */
     esp_err_t measure();
+
+    /**
+     * TODO: docs after methods polishing.
+     * @return
+     */
     esp_err_t send();
+
+    /**
+    * TODO: docs after methods polishing.
+    * @return
+    */
     esp_err_t sleep();
 
 };
