fef4ee29fd3d86f93a7078844ada8a76bfb17146
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-08-19 22:41:09 +0200

    docs(WeatherStation): complete documentation

--- firmware/include/WeatherStation.h
+++ firmware/include/WeatherStation.h
@@ -29,32 +29,50 @@ public:
     WeatherStation(i2c_master_bus_handle_t i2c_bus_handle, const char* brokerIp, uint16_t brokerPort): sensor_manager(i2c_bus_handle), mqtt_transport(brokerIp, brokerPort){};
 
     /**
-     * TODO: docs after methods polishing.
-     * @return
+     * @brief Initializes sensors via SensorManager.
+     *
+     * @return ESP_OK if initialization succeed.
+     * @return Many others ESP errors if something went wrong
      */
     esp_err_t init_sensors();
 
     /**
-     * TODO: docs after methods polishing.
-     * @return
+     * @brief Establishes communication.
+     *
+     * Initializes Wi-Fi via WiFiModule, then connects with the data transport broker e.g. MQTT.
+     *
+     * @return ESP_OK if initialization succeed.
+     * @return Other esp errors if something went wrong
      */
     esp_err_t init_communication();
 
     /**
-     * TODO: docs after methods polishing.
-     * @return
+     * @brief Takes measures and fills up Payload structure.
+     *
+     *
+     * @return ESP_OK if fetches data from at least one sensor.
+     * @return ESP_FAIL if all sensors are down.
      */
     esp_err_t measure();
 
     /**
-     * TODO: docs after methods polishing.
-     * @return
+     * @brief Sends payload via defined transport protocol.
+     *
+     * Serializes current data Payload, then initializes communication via init_communication() and uploads data via
+     * defined transport protocol.
+     *
+     * @return ESP_OK if initialization succeed.
+     * @return Other esp errors if something went wrong
      */
     esp_err_t send();
 
     /**
-    * TODO: docs after methods polishing.
-    * @return
+    * @brief Stops communication and then goes into deep sleep.
+    *
+    * @note This method will be modified/expanded so for now, documentation will be as simple as should be.
+    *
+    * @return ESP_OK if initialization succeed.
+    * @return Other esp errors if something went wrong
     */
     esp_err_t sleep();
 
