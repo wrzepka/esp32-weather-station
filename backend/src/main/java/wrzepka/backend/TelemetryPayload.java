f27a55a2e96bb354c597414b6773a276e9d0b55d
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-09-03 21:42:58 +0200

    docs(TelemetryPayload): document record class

--- backend/src/main/java/wrzepka/backend/TelemetryPayload.java
+++ backend/src/main/java/wrzepka/backend/TelemetryPayload.java
@@ -4,6 +4,15 @@ package wrzepka.backend;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+/**
+ * Record used for acquiring raw station measurements from MQTT protocol.
+ * It takes light intensity, temperature, humidity and pressure.
+ *
+ * @param lightIntensity Light intensity in lx.
+ * @param temperature Ambient temperature in Celsius.
+ * @param humidity Relative percentage humidity
+ * @param pressure Absolute pressure in pascals.
+ */
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TelemetryPayload(
         Long lightIntensity,
