dbd0995a1c363646ee59984105ac7f0d0a531159
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-09-05 13:27:18 +0200

    doc(WeatherTelemetryRepository): document repository

--- backend/src/main/java/wrzepka/backend/WeatherTelemetryRepository.java
+++ backend/src/main/java/wrzepka/backend/WeatherTelemetryRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * JPA repository, managing telemetry points.
+ * JPA repository, managing telemetry points. {@link WeatherTelemetry}
  */
 @Repository
 public interface WeatherTelemetryRepository extends JpaRepository<WeatherTelemetry, WeatherTelemetryId> {
