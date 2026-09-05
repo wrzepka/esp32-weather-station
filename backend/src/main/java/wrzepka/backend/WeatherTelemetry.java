71a4a56fdc46e1e949bc17fc2dc18823a865c0e5
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-09-05 13:19:27 +0200

    docs(WeatherTelemetry): document class

--- backend/src/main/java/wrzepka/backend/WeatherTelemetry.java
+++ backend/src/main/java/wrzepka/backend/WeatherTelemetry.java
@@ -5,28 +5,39 @@ import jakarta.persistence.*;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
+/**
+ * Entity represents single record of weather station telemetry.
+ * Mapped into timestamp table. {@code TelemetryMapper}
+ * Unique via complex key {@link WeatherTelemetryId}
+ */
 @Entity
 @Table(name = "weather_telemetry")
 @IdClass(WeatherTelemetryId.class)
 public class WeatherTelemetry {
 
+    /** Measurement timestamp SQL: TIMESTAMPTZ*/
     @Id
     @Column(name = "date_time", nullable = false)
     private OffsetDateTime dateTime;
 
+    /** Weather station identifier, downloaded from message topic: `DEVICECODE`_`MAC_ADDR`*/
     @Id
     @Column(name = "device_id", nullable = false)
     private String deviceId;
 
+    /** Absolute pressure in hPa, measured by BME280 sensor.*/
     @Column(name = "pressure")
     private Float pressure;
 
+    /** environment temperature in Celsius, measured by BME280 sensor.*/
     @Column(name = "temperature")
     private Float temperature;
 
+    /** Relative humidity in percentage, measured by BME280 sensor.*/
     @Column(name = "humidity")
     private Float humidity;
 
+    /** Light intensity in lx, measured by BH1750 sensor.*/
     @Column(name = "light_intensity")
     private Long lightIntensity;
 
